34cb8b1df30bce646eeaae7a38d16910440a6997
Andrei <andrei@nerdai.io>
2022-03-21 12:27:33 -0400

    cover up to  enums

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-console.log('Your code goes here!');
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+// JS object type annotation below
+// is not as optimal as letting TS do type inference
+// const person: {
+//    name: string;
+//    age: number;
+//}
+// however, there at times where you need to use
+// explicit type assignment, i.e., when TS can't infer
+// the difference for a tuple or an array
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'], // Array
+//     role: [2, 'author']
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
+}
--- app.ts
+++ app.ts
@@ -1,15 +1,40 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-} 
+// JS object type annotation below
+// is not as optimal as letting TS do type inference
+// const person: {
+//    name: string;
+//    age: number;
+//}
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
+// however, there at times where you need to use
+// explicit type assignment, i.e., when TS can't infer
+// the difference for a tuple or an array
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'], // Array
+//     role: [2, 'author']
+// };
 
-add(number1, number2, printResult, resultPhrase);
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'], // Array
+    role: Role.ADMIN
+};
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
+}
