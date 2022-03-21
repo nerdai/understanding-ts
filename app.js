9513df2b4d56417caa7520d222592039f5670f06
Andrei <andrei@nerdai.io>
2022-03-21 14:33:53 -0400

    cover section 2 up to aliases

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-// JS object type annotation below
-// is not as optimal as letting TS do type inference
-// const person: {
-//    name: string;
-//    age: number;
-//}
-// however, there at times where you need to use
-// explicit type assignment, i.e., when TS can't infer
-// the difference for a tuple or an array
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];
-// } = {
-//     name: 'Maximilian',
-//     age: 30,
-//     hobbies: ['Sports', 'Cooking'], // Array
-//     role: [2, 'author']
-// };
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+// with literal types       
+function combine(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
-}
+var combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
