f0b5fcf28a48a322c3a783f823bc48071b4b6e47
Andrei <andrei@nerdai.io>
2022-03-22 09:48:48 -0400

    complete section 3

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 const button = document.querySelector('button');
 
+function clickHandler(message: string) {
+  console.log('Clicked ' + message)
+}
+
+// can also us "optional" unwrapping with ! instead if you know button
+// does indeed exist
 if (button) {
-  button.addEventListener('click', () => {
-    console.log('Clicked');
-  })
+  button.addEventListener('click', clickHandler.bind(null, "You're welcome!"))
 }
