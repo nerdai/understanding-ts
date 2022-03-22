3c7dd0ab2331a16017d7fb2408b49ba552d57d7e
Andrei <andrei@nerdai.io>
2022-03-22 12:14:08 -0400

    add changes all the way up to, but no including inheritance video 65

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,35 @@
 "use strict";
 class Department {
-    constructor(n) {
-        this.name = n;
+    // constructor method
+    // constructor(n: string) {
+    //   this.name = n;
+    // }
+    // shorthand notation to remove duplication of declaration of fields
+    // and the constructor
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        // private name: string;
+        this.employees = []; // note public is default modifier
+    }
+    // other methods
+    describe() {
+        // note "this" keyword is not necessary
+        // but can be used to enforce more safe implementation
+        // only objects of Department class can call this method as a result
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Andrei');
 console.log(accounting);
+accounting.describe();
+accounting.printEmployeeInformation();
