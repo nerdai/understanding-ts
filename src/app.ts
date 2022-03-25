516a44a84cfe38e7bae37fccc99aa3cbc9f4fb68
Andrei <andrei@nerdai.io>
2022-03-25 15:06:14 -0400

    add axios library for making http requests to APIs

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,35 @@
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
+import axios from "axios";
 
-const GOOGLE_API_KEY = process.env.SECRET_KEY;
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   // send this to Google's API
-
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${process.env.SECRET_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(response);
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-form.addEventListener('submit', searchAddressHandler);
+form.addEventListener("submit", searchAddressHandler);
