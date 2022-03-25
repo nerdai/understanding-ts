c0ca3694d3a37ebcdb29b2bbbe42c7021a130055
Andrei <andrei@nerdai.io>
2022-03-25 10:28:48 -0400

    change index.html to point to bundle.js script

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       {
         test: /\.ts$/, // a reg exp to look for .ts
         use: 'ts-loader',
-        exclude: [/node_modules/, /_section2/]
+        exclude: [/node_modules/]          
       }
     ]
   },
