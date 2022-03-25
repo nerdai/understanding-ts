8270434747faab4f45f098192b3f6e996afa4691
Andrei <andrei@nerdai.io>
2022-03-25 15:47:11 -0400

    add google map types to remove need for declare var usage

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import axios from "axios";
+import { Loader } from "@googlemaps/js-api-loader";
 
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
-  status: 'OK' | 'ZERO_RESULTS';
+  status: "OK" | "ZERO_RESULTS";
 };
+
+const loader = new Loader({
+  apiKey: `${process.env.SECRET_KEY}`,
+  version: "weekly",
+});
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
@@ -20,11 +26,26 @@ function searchAddressHandler(event: Event) {
       )}&key=${process.env.SECRET_KEY}`
     )
     .then((response) => {
-      if (response.data.status !== 'OK') {
-        throw new Error('Could not fetch location!');
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
       }
       const coordinates = response.data.results[0].geometry.location;
-      console.log(response);
+      
+      // load map
+      let map: any;
+      loader.load().then(() => {
+        map = new google.maps.Map(
+          document.getElementById("map") as HTMLElement,
+          {
+            center: coordinates,
+            zoom: 12,
+          }
+        );
+        // add marker
+        new google.maps.Marker({ position: coordinates, map: map })
+      });
+
+      // console.log(response);
     })
     .catch((err) => {
       alert(err.message);
